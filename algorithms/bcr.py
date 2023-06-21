first bloc after cyclic reduction correct, need to fix backpropagation

algorithms/bcr.py
@@ -82,7 +82,7 @@ def block_cyclic_reduction(B, blocksize):
 
 
     F = np.identity(B.shape[0], dtype=B.dtype)
-    x = np.zeros((B.shape[0], B.shape[0]), dtype=B.dtype)
+    X = np.zeros((B.shape[0], B.shape[0]), dtype=B.dtype)
 
 
     # Compute reference solution before A being modified (work in place)
@@ -90,10 +90,6 @@ def block_cyclic_reduction(B, blocksize):
 
 
 
-    index1, index2, offset = 0, 0, 0
-    #alpha, gamma = 0.0, 0.0
-
-    #vizu.vizualiseDenseMatrixFlat(A, "A")
 
     alpha = np.zeros((blocksize, blocksize), dtype=B.dtype)
     gamma = np.zeros((blocksize, blocksize), dtype=B.dtype)
@@ -104,7 +100,7 @@ def block_cyclic_reduction(B, blocksize):
     nb_stages = int(math.log2(nblocks_padded + 1)) - 1 # number of stages of the cycle reduction
 
     for i_blockindex in range(nb_stages):
-        print("i: ", i_blockindex)
+        #print("i: ", i_blockindex)
         # i_blockindex is the curent stage of the cycle reduction
         for j_blockindex in range(int(math.pow(2, i_blockindex + 1)) - 1, nblocks_padded, int(math.pow(2, i_blockindex + 1))):
             # j_blockindex is the index of the row we are currently reducing towards
@@ -115,7 +111,7 @@ def block_cyclic_reduction(B, blocksize):
             index1_blockindex = j_blockindex - offset_blockindex
             index2_blockindex = j_blockindex + offset_blockindex
 
-            print("  j_blockindex: ", j_blockindex, "offset_blockindex: ", offset_blockindex, "index1_blockindex: ", index1_blockindex, "index2_blockindex: ", index2_blockindex)
+            #print("  j_blockindex: ", j_blockindex, "offset_blockindex: ", offset_blockindex, "index1_blockindex: ", index1_blockindex, "index2_blockindex: ", index2_blockindex)
 
             # Pre-computing of row-based indices
             j_rowindex   = j_blockindex * blocksize
@@ -126,8 +122,6 @@ def block_cyclic_reduction(B, blocksize):
             
             index2_rowindex   = index2_blockindex * blocksize
             index2p1_rowindex = (index2_blockindex + 1) * blocksize
-
-            #vizu.vizualiseDenseMatrixFlat(B[index1_rowindex:index1p1_rowindex, index1_rowindex:index1p1_rowindex], "B[index1_rowindex:index1p1_rowindex, index1_rowindex:index1p1_rowindex]")
 
 
             alpha = B[j_rowindex:jp1_rowindex, index1_rowindex:index1p1_rowindex]\
@@ -150,61 +144,53 @@ def block_cyclic_reduction(B, blocksize):
                                                                         - ( alpha @ F[index1_rowindex:index1p1_rowindex, k_rowindex:kp1_rowindex]\
                                                                             + gamma @ F[index2_rowindex:index2p1_rowindex, k_rowindex:kp1_rowindex] )
 
-            vizu.vizualiseDenseMatrixFlat(B, f"B j: {j_blockindex}")
-
-
-
-    """ # Cycle reduction
-    for i in range(int(math.log2(size + 1)) - 1):
-        print("i: ", i)
-        # range(int(math.log2(size + 1)) - 1) is the number of stages of the cycle reduction
-        # i is the curent stage of the cycle reduction
-        for j in range(int(math.pow(2, i + 1)) - 1, size, int(math.pow(2, i + 1))):
-            # j is the index of the row we are currently reducing towards
-            # offset is the distance between the two rows we are reducing from
-            # index1 and index2 are the indices of the rows we are reducing from
-            #   - index1 being the upper row and index2 the lower row
-            offset = int(math.pow(2, i)) 
-            index1 = j - offset
-            index2 = j + offset
-
-            print("  j: ", j, "offset: ", offset, "index1: ", index1, "index2: ", index2)
-
-
-            alpha = A[j][index1] / A[index1][index1]
-            gamma = A[j][index2] / A[index2][index2]
-
-            for k in range(size):
-                print("     k: ", k)
-                A[j][k] -= alpha * A[index1][k] + gamma * A[index2][k]
-                F[j][k] -= alpha * F[index1][k] + gamma * F[index2][k] """
-
-
     vizu.compareDenseMatrix(B, F, "F")
 
-    """ # Back substitution
-    index = (size - 1) // 2
-    x[index] = F[index] / A[index][index]
+    # Back substitution
+    index_blockindex = (nblocks_padded - 1) // 2
+    index_rowindex   = index_blockindex * blocksize
+    indexp1_rowindex = (index_blockindex + 1) * blocksize
+    X[index_rowindex:indexp1_rowindex, index_rowindex:indexp1_rowindex] = F[index_rowindex:indexp1_rowindex, index_rowindex:indexp1_rowindex] @ np.linalg.inv(B[index_rowindex:indexp1_rowindex, index_rowindex:indexp1_rowindex])
 
-    for i in range(int(math.log2(size + 1)) - 2, -1, -1):
-        for j in range(int(math.pow(2, i + 1)) - 1, size, int(math.pow(2, i + 1))):
-            offset = int(math.pow(2, i))
-            index1 = j - offset
-            index2 = j + offset
+    for i in range(int(math.log2(nblocks_padded + 1)) - 2, -1, -1):
+        for j in range(int(math.pow(2, i + 1)) - 1, nblocks_padded, int(math.pow(2, i + 1))):
+            offset_blockindex = int(math.pow(2, i))
+            index1_blockindex = j - offset_blockindex
+            index2_blockindex = j + offset_blockindex
 
-            x[index1] = F[index1]
-            x[index2] = F[index2]
+            # Pre-computing of row-based indices
+            j_rowindex   = j_blockindex * blocksize
+            jp1_rowindex = (j_blockindex + 1) * blocksize
 
-            for k in range(size):
-                if k != index1:
-                    x[index1] -= A[index1][k] * x[k]
-                if k != index2:
-                    x[index2] -= A[index2][k] * x[k]
+            index1_rowindex   = index1_blockindex * blocksize
+            index1p1_rowindex = (index1_blockindex + 1) * blocksize
+            
+            index2_rowindex   = index2_blockindex * blocksize
+            index2p1_rowindex = (index2_blockindex + 1) * blocksize
 
-            x[index1] /= A[index1][index1]
-            x[index2] /= A[index2][index2] """
+            
+            X[index1_rowindex:index1p1_rowindex, index1_rowindex:index1p1_rowindex] = F[index1_rowindex:index1p1_rowindex, index1_rowindex:index1p1_rowindex]
+            X[index2_rowindex:index2p1_rowindex, index1_rowindex:index1p1_rowindex] = F[index2_rowindex:index2p1_rowindex, index1_rowindex:index1p1_rowindex]
 
 
-    vizu.compareDenseMatrix(npinvert, B, "A_ref VS A")
+            for k_blockindex in range(nblocks_padded):
+                k_rowindex   = k_blockindex * blocksize
+                kp1_rowindex = (k_blockindex + 1) * blocksize
+
+                if k_blockindex != index1_blockindex:
+                    X[index1_rowindex:index1p1_rowindex, k_rowindex:kp1_rowindex] = X[index1_rowindex:index1p1_rowindex, k_rowindex:kp1_rowindex]\
+                                                            - (B[index1_rowindex:index1p1_rowindex, k_rowindex:kp1_rowindex]\
+                                                                @ X[k_rowindex:kp1_rowindex, k_rowindex:kp1_rowindex])
+                if k_blockindex != index2_blockindex:
+                    X[index2_rowindex:index2p1_rowindex, k_rowindex:kp1_rowindex] = X[index2_rowindex:index2p1_rowindex, k_rowindex:kp1_rowindex]\
+                                                            - (B[index2_rowindex:index2p1_rowindex, k_rowindex:kp1_rowindex] 
+                                                                @ X[k_rowindex:kp1_rowindex, k_rowindex:kp1_rowindex])
+
+
+                X[index1_rowindex:index1p1_rowindex, k_rowindex:kp1_rowindex] = X[index1_rowindex:index1p1_rowindex, k_rowindex:kp1_rowindex] @ np.linalg.inv(B[index1_rowindex:index1p1_rowindex, index1_rowindex:index1p1_rowindex])
+                X[index2_rowindex:index2p1_rowindex, k_rowindex:kp1_rowindex] = X[index2_rowindex:index2p1_rowindex, k_rowindex:kp1_rowindex] @ np.linalg.inv(B[index2_rowindex:index2p1_rowindex, index2_rowindex:index2p1_rowindex])
+
+
+    vizu.compareDenseMatrix(npinvert, X, "A_ref VS B")
 
 
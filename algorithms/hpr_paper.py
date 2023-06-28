7fcea7e5e782288caa429ef9a977cfa960a92954
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2023-06-28 11:00:42 +0200

    Corrected indices in Schur decomposition HPR + implemented produce_schur (non-tested)

--- algorithms/hpr_paper.py
+++ algorithms/hpr_paper.py
@@ -22,67 +22,154 @@ from mpi4py import MPI
 
 
 
-
-def reduce_schur(A, top_blockrow, bottom_blockrow, blocksize):
-
-    comm = MPI.COMM_WORLD
-    comm_rank = comm.Get_rank()
-    comm_size = comm.Get_size()
+""" 
+    Schur reductions functions
+"""
+def reduce_schur_topleftcorner(A, top_blockrow, bottom_blockrow, blocksize):
 
     nblocks = A.shape[0] // blocksize
     
     L = np.zeros((nblocks*blocksize, nblocks*blocksize), dtype=A.dtype)
     U = np.zeros((nblocks*blocksize, nblocks*blocksize), dtype=A.dtype)
 
-    if comm_rank == 0 and comm_size > 1:
-        # Corner elimination downward
-        for i_blockrow in range(top_blockrow+1, bottom_blockrow):
-            im1_rowindice = (i_blockrow-1)*blocksize
-            i_rowindice   = i_blockrow*blocksize
-            ip1_rowindice = (i_blockrow+1)*blocksize
+    # Corner elimination downward
+    for i_blockrow in range(top_blockrow+1, bottom_blockrow):
+        im1_rowindice = (i_blockrow-1)*blocksize
+        i_rowindice   = i_blockrow*blocksize
+        ip1_rowindice = (i_blockrow+1)*blocksize
 
-            A_inv_im1_im1 = np.linalg.inv(A[im1_rowindice:i_rowindice, im1_rowindice:i_rowindice])
+        A_inv_im1_im1 = np.linalg.inv(A[im1_rowindice:i_rowindice, im1_rowindice:i_rowindice])
 
-            L[i_rowindice:ip1_rowindice, im1_rowindice:i_rowindice]  = A[i_rowindice:ip1_rowindice, im1_rowindice:i_rowindice] @ A_inv_im1_im1
-            U[im1_rowindice:i_rowindice, i_rowindice:ip1_rowindice]  = A_inv_im1_im1 @ A[im1_rowindice:i_rowindice, i_rowindice:ip1_rowindice]
-            A[i_rowindice:ip1_rowindice, i_rowindice:ip1_rowindice] -= L[i_rowindice:ip1_rowindice, im1_rowindice:i_rowindice] @ A[im1_rowindice:i_rowindice, i_rowindice:ip1_rowindice]
+        L[i_rowindice:ip1_rowindice, im1_rowindice:i_rowindice]  = A[i_rowindice:ip1_rowindice, im1_rowindice:i_rowindice] @ A_inv_im1_im1
+        U[im1_rowindice:i_rowindice, i_rowindice:ip1_rowindice]  = A_inv_im1_im1 @ A[im1_rowindice:i_rowindice, i_rowindice:ip1_rowindice]
+        A[i_rowindice:ip1_rowindice, i_rowindice:ip1_rowindice] -= L[i_rowindice:ip1_rowindice, im1_rowindice:i_rowindice] @ A[im1_rowindice:i_rowindice, i_rowindice:ip1_rowindice]
     
-    if comm_rank == comm_size-1:
-        # Corner elimination upward
-        for i_blockrow in range(bottom_blockrow-2, top_blockrow, -1):
-            i_rowindice   = i_blockrow*blocksize
-            ip1_rowindice = (i_blockrow+1)*blocksize
-            ip2_rowindice = (i_blockrow+2)*blocksize
+    return A, L, U
 
-            A_inv_ip1_ip1 = np.linalg.inv(A[ip1_rowindice:ip2_rowindice, ip1_rowindice:ip2_rowindice])
 
-            L[i_rowindice:ip1_rowindice, ip1_rowindice:ip2_rowindice] = A[i_rowindice:ip1_rowindice, ip1_rowindice:ip2_rowindice] @ A_inv_ip1_ip1
-            U[ip1_rowindice:ip2_rowindice, i_rowindice:ip1_rowindice] = A_inv_ip1_ip1 @ A[ip1_rowindice:ip2_rowindice, i_rowindice:ip1_rowindice]
-            A[i_rowindice:ip1_rowindice, i_rowindice:ip1_rowindice]  -= L[i_rowindice:ip1_rowindice, ip1_rowindice:ip2_rowindice] @ A[ip1_rowindice:ip2_rowindice, i_rowindice:ip1_rowindice]
+def reduce_schur_bottomrightcorner(A, top_blockrow, bottom_blockrow, blocksize):
+
+    nblocks = A.shape[0] // blocksize
     
-    if comm_rank != 0 and comm_rank != comm_size-1 and comm_size > 1:
-        # Center elimination downward
-        for i_blockrow in range(top_blockrow+1, bottom_blockrow):
-            im1_rowindice = (i_blockrow-1)*blocksize
-            i_rowindice   = i_blockrow*blocksize
-            ip1_rowindice = (i_blockrow+1)*blocksize
-            top_rowindice   = top_blockrow*blocksize
-            topp1_rowindice = (top_blockrow+1)*blocksize
+    L = np.zeros((nblocks*blocksize, nblocks*blocksize), dtype=A.dtype)
+    U = np.zeros((nblocks*blocksize, nblocks*blocksize), dtype=A.dtype)
 
-            A_inv_im1_im1 = np.linalg.inv(A[im1_rowindice:i_rowindice, im1_rowindice:i_rowindice])
+    # Corner elimination upward
+    for i_blockrow in range(bottom_blockrow-2, top_blockrow-1, -1):
+        i_rowindice   = i_blockrow*blocksize
+        ip1_rowindice = (i_blockrow+1)*blocksize
+        ip2_rowindice = (i_blockrow+2)*blocksize
 
-            L[i_rowindice:ip1_rowindice, im1_rowindice:i_rowindice]     = A[i_rowindice:ip1_rowindice, im1_rowindice:i_rowindice] @ A_inv_im1_im1
-            L[top_rowindice:topp1_rowindice, im1_rowindice:i_rowindice] = A[top_rowindice:topp1_rowindice, im1_rowindice:i_rowindice] @ A_inv_im1_im1
-            U[im1_rowindice:i_rowindice, i_rowindice:ip1_rowindice]     = A_inv_im1_im1 @ A[im1_rowindice:i_rowindice, i_rowindice:ip1_rowindice]
-            U[im1_rowindice:i_rowindice, top_rowindice:topp1_rowindice] = A_inv_im1_im1 @ A[im1_rowindice:i_rowindice, top_rowindice:topp1_rowindice]
-            
-            A[i_rowindice:ip1_rowindice, i_rowindice:ip1_rowindice]         -= L[i_rowindice:ip1_rowindice, im1_rowindice:i_rowindice] @ A[im1_rowindice:i_rowindice, i_rowindice:ip1_rowindice]
-            A[top_rowindice:topp1_rowindice, top_rowindice:topp1_rowindice] -= L[top_rowindice:topp1_rowindice, im1_rowindice:i_rowindice] @ A[im1_rowindice:i_rowindice, top_rowindice:topp1_rowindice]
-            A[i_rowindice:ip1_rowindice, top_rowindice:topp1_rowindice]     -= L[i_rowindice:ip1_rowindice, im1_rowindice:i_rowindice] @ A[im1_rowindice:i_rowindice, top_rowindice:topp1_rowindice]
-            A[top_rowindice:topp1_rowindice, i_rowindice:ip1_rowindice]     -= L[top_rowindice:topp1_rowindice, im1_rowindice:i_rowindice] @ A[im1_rowindice:i_rowindice, i_rowindice:ip1_rowindice]
+        A_inv_ip1_ip1 = np.linalg.inv(A[ip1_rowindice:ip2_rowindice, ip1_rowindice:ip2_rowindice])
+
+        L[i_rowindice:ip1_rowindice, ip1_rowindice:ip2_rowindice] = A[i_rowindice:ip1_rowindice, ip1_rowindice:ip2_rowindice] @ A_inv_ip1_ip1
+        U[ip1_rowindice:ip2_rowindice, i_rowindice:ip1_rowindice] = A_inv_ip1_ip1 @ A[ip1_rowindice:ip2_rowindice, i_rowindice:ip1_rowindice]
+        A[i_rowindice:ip1_rowindice, i_rowindice:ip1_rowindice]  -= L[i_rowindice:ip1_rowindice, ip1_rowindice:ip2_rowindice] @ A[ip1_rowindice:ip2_rowindice, i_rowindice:ip1_rowindice]
 
     return A, L, U
 
+
+def reduce_schur_center(A, top_blockrow, bottom_blockrow, blocksize):
+
+    nblocks = A.shape[0] // blocksize
+    
+    L = np.zeros((nblocks*blocksize, nblocks*blocksize), dtype=A.dtype)
+    U = np.zeros((nblocks*blocksize, nblocks*blocksize), dtype=A.dtype)
+
+    # Center elimination downward
+    for i_blockrow in range(top_blockrow+2, bottom_blockrow):
+        im1_rowindice = (i_blockrow-1)*blocksize
+        i_rowindice   = i_blockrow*blocksize
+        ip1_rowindice = (i_blockrow+1)*blocksize
+
+        top_rowindice   = top_blockrow*blocksize
+        topp1_rowindice = (top_blockrow+1)*blocksize
+
+        A_inv_im1_im1 = np.linalg.inv(A[im1_rowindice:i_rowindice, im1_rowindice:i_rowindice])
+
+        L[i_rowindice:ip1_rowindice, im1_rowindice:i_rowindice]     = A[i_rowindice:ip1_rowindice, im1_rowindice:i_rowindice] @ A_inv_im1_im1
+        L[top_rowindice:topp1_rowindice, im1_rowindice:i_rowindice] = A[top_rowindice:topp1_rowindice, im1_rowindice:i_rowindice] @ A_inv_im1_im1
+        U[im1_rowindice:i_rowindice, i_rowindice:ip1_rowindice]     = A_inv_im1_im1 @ A[im1_rowindice:i_rowindice, i_rowindice:ip1_rowindice]
+        U[im1_rowindice:i_rowindice, top_rowindice:topp1_rowindice] = A_inv_im1_im1 @ A[im1_rowindice:i_rowindice, top_rowindice:topp1_rowindice]
+        
+        A[i_rowindice:ip1_rowindice, i_rowindice:ip1_rowindice]         -= L[i_rowindice:ip1_rowindice, im1_rowindice:i_rowindice] @ A[im1_rowindice:i_rowindice, i_rowindice:ip1_rowindice]
+        A[top_rowindice:topp1_rowindice, top_rowindice:topp1_rowindice] -= L[top_rowindice:topp1_rowindice, im1_rowindice:i_rowindice] @ A[im1_rowindice:i_rowindice, top_rowindice:topp1_rowindice]
+        A[i_rowindice:ip1_rowindice, top_rowindice:topp1_rowindice]     -= L[i_rowindice:ip1_rowindice, im1_rowindice:i_rowindice] @ A[im1_rowindice:i_rowindice, top_rowindice:topp1_rowindice]
+        A[top_rowindice:topp1_rowindice, i_rowindice:ip1_rowindice]     -= L[top_rowindice:topp1_rowindice, im1_rowindice:i_rowindice] @ A[im1_rowindice:i_rowindice, i_rowindice:ip1_rowindice]
+
+    return A, L, U
+
+
+
+""" 
+    Schur production functions
+"""
+def produce_schur_topleftcorner(A, L, U, G, top_blockrow, bottom_blockrow, blocksize):
+
+    # Corner produce upwards
+    for i in range(bottom_blockrow-1, top_blockrow-1, -1):
+        im1_rowindice = (i-1)*blocksize
+        i_rowindice   = i*blocksize
+        ip1_rowindice = (i+1)*blocksize
+
+        G[i_rowindice:ip1_rowindice, im1_rowindice:i_rowindice] = -G[i_rowindice:ip1_rowindice, i_rowindice:ip1_rowindice] @ L[i_rowindice:ip1_rowindice, im1_rowindice:i_rowindice]
+        G[im1_rowindice:i_rowindice, i_rowindice:ip1_rowindice] = -U[im1_rowindice:i_rowindice, i_rowindice:ip1_rowindice] @ G[i_rowindice:ip1_rowindice, i_rowindice:ip1_rowindice]
+        G[im1_rowindice:i_rowindice, im1_rowindice:i_rowindice] = np.linalg.inv(A[im1_rowindice:i_rowindice, im1_rowindice:i_rowindice]) - U[im1_rowindice:i_rowindice, i_rowindice:ip1_rowindice] @ G[i_rowindice:ip1_rowindice, im1_rowindice:i_rowindice]
+
+    return G
+
+
+def produce_schur_bottomrightcorner(A, L, U, G, top_blockrow, bottom_blockrow, blocksize):
+
+    # Corner produce downwards
+    for i in range(top_blockrow, bottom_blockrow-1):
+        i_rowindice   = i*blocksize
+        ip1_rowindice = (i+1)*blocksize
+        ip2_rowindice = (i+2)*blocksize
+
+        G[i_rowindice:ip1_rowindice, ip1_rowindice:ip2_rowindice]   = -G[i_rowindice:ip1_rowindice, i_rowindice:ip1_rowindice] @ L[i_rowindice:ip1_rowindice, ip1_rowindice:ip2_rowindice]
+        G[ip1_rowindice:ip2_rowindice, i_rowindice:ip1_rowindice]   = -U[ip1_rowindice:ip2_rowindice, i_rowindice:ip1_rowindice] @ G[i_rowindice:ip1_rowindice, i_rowindice:ip1_rowindice]
+        G[ip1_rowindice:ip2_rowindice, ip1_rowindice:ip2_rowindice] = np.linalg.inv(A[ip1_rowindice:ip2_rowindice, ip1_rowindice:ip2_rowindice]) - U[ip1_rowindice:ip2_rowindice, i_rowindice:ip1_rowindice] @ G[i_rowindice:ip1_rowindice, ip1_rowindice:ip2_rowindice]
+
+    return G
+
+
+def produce_schur_center(A, L, U, G, top_blockrow, bottom_blockrow, blocksize):
+
+    # Center produce upwards
+    top_rowindice   = top_blockrow*blocksize
+    topp1_rowindice = (top_blockrow+1)*blocksize
+    topp2_rowindice = (top_blockrow+2)*blocksize
+    topp3_rowindice = (top_blockrow+3)*blocksize
+
+    botm1_rowindice = (bottom_blockrow-2)*blocksize
+    bot_rowindice   = (bottom_blockrow-1)*blocksize
+    botp1_rowindice = bottom_blockrow*blocksize
+
+    G[bot_rowindice:botp1_rowindice, botm1_rowindice:bot_rowindice] = -G[bot_rowindice:botp1_rowindice, top_rowindice:topp1_rowindice] @ L[top_rowindice:topp1_rowindice, botm1_rowindice:bot_rowindice] - G[bot_rowindice:botp1_rowindice, bot_rowindice:botp1_rowindice] @ L[bot_rowindice:botp1_rowindice, botm1_rowindice:bot_rowindice]
+    G[botm1_rowindice:bot_rowindice, bot_rowindice:botp1_rowindice] = -U[botm1_rowindice:bot_rowindice, bot_rowindice:botp1_rowindice] @ G[bot_rowindice:botp1_rowindice, bot_rowindice:botp1_rowindice] - U[botm1_rowindice:bot_rowindice, top_rowindice:topp1_rowindice] @ G[top_rowindice:topp1_rowindice, bot_rowindice:botp1_rowindice]
+
+    for i in range(bottom_blockrow-2, top_blockrow, -1):
+        i_rowindice   = i*blocksize
+        ip1_rowindice = (i+1)*blocksize
+        ip2_rowindice = (i+2)*blocksize
+
+        G[top_rowindice:topp1_rowindice, i_rowindice:ip1_rowindice] = -G[top_rowindice:topp1_rowindice, top_rowindice:topp1_rowindice] @ L[top_rowindice:topp1_rowindice, i_rowindice:ip1_rowindice] - G[top_rowindice:topp1_rowindice, ip1_rowindice:ip2_rowindice] @ L[ip1_rowindice:ip2_rowindice, i_rowindice:ip1_rowindice]
+        G[i_rowindice:ip1_rowindice, top_rowindice:topp1_rowindice] = -U[i_rowindice:ip1_rowindice, ip1_rowindice:ip2_rowindice] @ G[ip1_rowindice:ip2_rowindice, top_rowindice:topp1_rowindice] - U[i_rowindice:ip1_rowindice, top_rowindice:topp1_rowindice] @ G[top_rowindice:topp1_rowindice, top_rowindice:topp1_rowindice]
+
+    for i in range(bottom_blockrow-2, top_blockrow+1, -1):
+        im1_rowindice = (i-1)*blocksize
+        i_rowindice   = i*blocksize
+        ip1_rowindice = (i+1)*blocksize
+        ip2_rowindice = (i+2)*blocksize
+
+        G[i_rowindice:ip1_rowindice, i_rowindice:ip1_rowindice] = np.linalg.inv(A[i_rowindice:ip1_rowindice, i_rowindice:ip1_rowindice]) - U[i_rowindice:ip1_rowindice, top_rowindice:topp1_rowindice] @ G[top_rowindice:topp1_rowindice, i_rowindice:ip1_rowindice] - U[i_rowindice:ip1_rowindice, ip1_rowindice:ip2_rowindice] @ G[ip1_rowindice:ip2_rowindice, i_rowindice:ip1_rowindice] 
+        G[im1_rowindice:i_rowindice, i_rowindice:ip1_rowindice] = -U[im1_rowindice:i_rowindice, top_rowindice:topp1_rowindice] @ G[top_rowindice:topp1_rowindice, i_rowindice:ip1_rowindice] - U[im1_rowindice:i_rowindice, i_rowindice:ip1_rowindice] @ G[i_rowindice:ip1_rowindice, i_rowindice:ip1_rowindice]
+        G[i_rowindice:i_rowindice, im1_rowindice:i_rowindice]   = -G[i_rowindice:ip1_rowindice, top_rowindice:topp1_rowindice] @ L[top_rowindice:topp1_rowindice, im1_rowindice:i_rowindice] - G[i_rowindice:ip1_rowindice, i_rowindice:ip1_rowindice] @ L[i_rowindice:ip1_rowindice, im1_rowindice:i_rowindice]
+
+    G[topp1_rowindice:topp2_rowindice, topp1_rowindice:topp2_rowindice] = np.linalg.inv(A[topp1_rowindice:topp2_rowindice, topp1_rowindice:topp2_rowindice]) - U[topp1_rowindice:topp2_rowindice, top_rowindice:topp1_rowindice] @ G[top_rowindice:topp1_rowindice, topp1_rowindice:topp2_rowindice] - U[topp1_rowindice:topp2_rowindice, topp2_rowindice:topp3_rowindice] @ G[topp2_rowindice:topp3_rowindice, topp1_rowindice:topp2_rowindice]
+
+    return G
 
 
 
@@ -110,7 +197,7 @@ def inverse_hybrid(A, blocksize):
         top_blockrow     = 0
         bottom_blockrow  = nblocks // comm_size
 
-        A, L, U = reduce_schur(A, top_blockrow, bottom_blockrow, blocksize)
+        A, L, U = reduce_schur_topleftcorner(A, top_blockrow, bottom_blockrow, blocksize)
 
 
         # Gather Schur reduction on process 0
@@ -149,7 +236,7 @@ def inverse_hybrid(A, blocksize):
         top_blockrow     = comm_rank * (nblocks // comm_size)
         bottom_blockrow  = nblocks
 
-        A, L, U = reduce_schur(A, top_blockrow, bottom_blockrow, blocksize)
+        A, L, U = reduce_schur_bottomrightcorner(A, top_blockrow, bottom_blockrow, blocksize)
 
         # Send Schur reduction to process 0
         top_row    = top_blockrow * blocksize
@@ -164,7 +251,7 @@ def inverse_hybrid(A, blocksize):
         top_blockrow     = comm_rank * (nblocks // comm_size)
         bottom_blockrow  = (comm_rank+1) * (nblocks // comm_size)
 
-        A, L, U = reduce_schur(A, top_blockrow, bottom_blockrow, blocksize)
+        A, L, U = reduce_schur_center(A, top_blockrow, bottom_blockrow, blocksize)
 
         # Send Schur reduction to process 0
         top_row    = top_blockrow * blocksize

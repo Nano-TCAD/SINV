384635b07647d3e2f96647235f10e575f79fc84d
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2023-06-28 16:34:48 +0200

    Added Schur to bcr mpi communication

--- algorithms/hpr_paper.py
+++ algorithms/hpr_paper.py
@@ -7,9 +7,9 @@
 Copyright 2023 ETH Zurich and the QuaTrEx authors. All rights reserved.
 """
 
-import utils.vizualisation       as vizu
+import utils.vizualisation as vizu
 
-import algorithms.bcr            as bcr
+import algorithms.bcr      as bcr
 
 import numpy as np
 import time
@@ -186,6 +186,7 @@ def inverse_hybrid(A, blocksize):
     bottom_blockrow  = 0
 
 
+
     # Phase 1. Schur reduction 
     if comm_rank == 0:
         # First / top process
@@ -209,26 +210,28 @@ def inverse_hybrid(A, blocksize):
         A, L, U = reduce_schur_center(A, top_blockrow, bottom_blockrow, blocksize)
 
 
-    #print("comm_size = {}".format(comm_size))
-    #print("Process {} owns rows [{} : {}[".format(comm_rank, top_blockrow, bottom_blockrow))
-
 
     # Phase 2. BCR reduction
     nblocks_bcr_system = (comm_size-1) * 2
 
-    #print("nblocks_bcr_system = {}".format(nblocks_bcr_system))
-
-    A_bcr = np.zeros((nblocks_bcr_system*blocksize, nblocks*blocksize), dtype=A.dtype)
-    L_bcr = np.zeros((nblocks_bcr_system*blocksize, nblocks*blocksize), dtype=A.dtype)
-    U_bcr = np.zeros((nblocks_bcr_system*blocksize, nblocks*blocksize), dtype=A.dtype)
-
+    A_bcr = np.zeros((nblocks_bcr_system*blocksize, nblocks_bcr_system*blocksize), dtype=A.dtype)
+    L_bcr = np.zeros((nblocks_bcr_system*blocksize, nblocks_bcr_system*blocksize), dtype=A.dtype)
+    U_bcr = np.zeros((nblocks_bcr_system*blocksize, nblocks_bcr_system*blocksize), dtype=A.dtype)
 
     if comm_rank == 0:
         # Initialize first row of A_bcr with the Schur reduction of the first process, (1 row)
         bottom_rowindice   = (bottom_blockrow-1) * blocksize
         bottomp1_rowindice = bottom_blockrow * blocksize
 
-        A_bcr[0:blocksize, :] = A[bottom_rowindice:bottomp1_rowindice, :]
+        nblocks_process = bottom_blockrow - top_blockrow
+
+        first_blockcol = nblocks_process - 1
+        last_blockcol  = nblocks_bcr_system + first_blockcol
+        first_colindice = first_blockcol * blocksize
+        last_colindice  = last_blockcol * blocksize
+
+        A_bcr[0:blocksize, :] = A[bottom_rowindice:bottomp1_rowindice, first_colindice:last_colindice]
+        L_bcr[0:blocksize, :] = L[bottom_rowindice:bottomp1_rowindice, first_colindice:last_colindice]
 
         # Receive the Schur reduction from the central process, (2 rows)
         for i in range(1, comm_size-1):
@@ -236,37 +239,90 @@ def inverse_hybrid(A, blocksize):
             ip2_rowindice = i_rowindice + 2 * blocksize
             
             A_bcr[i_rowindice:ip2_rowindice, :] = comm.recv(source=i, tag=0)
+            L_bcr[i_rowindice:ip2_rowindice, :] = comm.recv(source=i, tag=1)
+            U_bcr[i_rowindice:ip2_rowindice, :] = comm.recv(source=i, tag=2)
 
         # Receive the Schur reduction from the last process, (1 row)
         last_rowindice   = (nblocks_bcr_system-1)*blocksize
         lastp1_rowindice = nblocks_bcr_system*blocksize
 
         A_bcr[last_rowindice:lastp1_rowindice, :] = comm.recv(source=comm_size-1, tag=0)
+        L_bcr[last_rowindice:lastp1_rowindice, :] = comm.recv(source=comm_size-1, tag=1)
+        U_bcr[last_rowindice:lastp1_rowindice, :] = comm.recv(source=comm_size-1, tag=2)
 
+
+        # Compute the BCR reduction of the aggregated system
         vizu.vizualiseDenseMatrixFlat(A_bcr, "A_bcr")
+
+        G_bcr = bcr.inverse_bcr(A_bcr, blocksize)
+
+        vizu.vizualiseDenseMatrixFlat(G_bcr, "G_bcr")
+
 
     elif comm_rank == comm_size-1:
         # Last process send his Schur reduced rows to process 0 
         top_rowindice   = top_blockrow * blocksize
         topp1_rowindice = (top_blockrow+1) * blocksize
 
-        comm.send(A[top_rowindice:topp1_rowindice, :], dest=0, tag=0)
+        nblocks_process = bottom_blockrow - top_blockrow
+
+        last_blockcol  = nblocks - (nblocks_process-1)
+        first_blockcol = last_blockcol - nblocks_bcr_system
+        first_colindice = first_blockcol * blocksize
+        last_colindice  = last_blockcol * blocksize
+
+        comm.send(A[top_rowindice:topp1_rowindice, first_colindice:last_colindice], dest=0, tag=0)
+        comm.send(L[top_rowindice:topp1_rowindice, first_colindice:last_colindice], dest=0, tag=1)
+        comm.send(U[top_rowindice:topp1_rowindice, first_colindice:last_colindice], dest=0, tag=2)
 
     else:
         # Middle process send his Schur reduced rows to process 0 
+        A_reduced = np.zeros((2*blocksize, nblocks_bcr_system*blocksize), dtype=A.dtype)
+        L_reduced = np.zeros((2*blocksize, nblocks_bcr_system*blocksize), dtype=A.dtype)
+        U_reduced = np.zeros((2*blocksize, nblocks_bcr_system*blocksize), dtype=A.dtype)
+
+        # Rows strides
         top_rowindice   = top_blockrow * blocksize
         topp1_rowindice = (top_blockrow+1) * blocksize
 
         bottom_rowindice   = (bottom_blockrow-1) * blocksize
         bottomp1_rowindice = bottom_blockrow * blocksize
 
-        A_reduced = np.zeros((2*blocksize, nblocks*blocksize), dtype=A.dtype)
-        A_reduced[0:blocksize, :]           = A[top_rowindice:topp1_rowindice, :]
-        A_reduced[blocksize:2*blocksize, :] = A[bottom_rowindice:bottomp1_rowindice, :]
+        # Columns strides
+        nblocks_process = bottom_blockrow - top_blockrow
+
+        start1_blockcol  = comm_rank * (nblocks // comm_size) - 1
+        stop1_blockcol   = start1_blockcol + 2
+        start1_colindice = start1_blockcol * blocksize
+        stop1_colindice  = stop1_blockcol * blocksize
+
+        start2_blockcol = stop1_blockcol + (nblocks_process-2)
+        stop2_blockcol  = start2_blockcol + 2
+        start2_colindice = start2_blockcol * blocksize
+        stop2_colindice  = stop2_blockcol * blocksize
+
+        # Row 0, Col 0
+        A_reduced[0:blocksize, 0:2*blocksize]           = A[top_rowindice:topp1_rowindice, start1_colindice:stop1_colindice]
+        L_reduced[0:blocksize, 0:2*blocksize]           = L[top_rowindice:topp1_rowindice, start1_colindice:stop1_colindice]
+        U_reduced[0:blocksize, 0:2*blocksize]           = U[top_rowindice:topp1_rowindice, start1_colindice:stop1_colindice]
+
+        # Row 1, Col 0
+        A_reduced[blocksize:2*blocksize, 0:2*blocksize] = A[bottom_rowindice:bottomp1_rowindice, start1_colindice:stop1_colindice]
+        L_reduced[blocksize:2*blocksize, 0:2*blocksize] = L[bottom_rowindice:bottomp1_rowindice, start1_colindice:stop1_colindice]
+        U_reduced[blocksize:2*blocksize, 0:2*blocksize] = U[bottom_rowindice:bottomp1_rowindice, start1_colindice:stop1_colindice]
+
+        # Row 0, Col 1
+        A_reduced[0:blocksize, 2*blocksize:]           = A[top_rowindice:topp1_rowindice, start2_colindice:stop2_colindice]
+        L_reduced[0:blocksize, 2*blocksize:]           = L[top_rowindice:topp1_rowindice, start2_colindice:stop2_colindice]
+        U_reduced[0:blocksize, 2*blocksize:]           = U[top_rowindice:topp1_rowindice, start2_colindice:stop2_colindice]
+
+        # Row 1, Col 1
+        A_reduced[blocksize:2*blocksize, 2*blocksize:] = A[bottom_rowindice:bottomp1_rowindice, start2_colindice:stop2_colindice]
+        L_reduced[blocksize:2*blocksize, 2*blocksize:] = L[bottom_rowindice:bottomp1_rowindice, start2_colindice:stop2_colindice]
+        U_reduced[blocksize:2*blocksize, 2*blocksize:] = U[bottom_rowindice:bottomp1_rowindice, start2_colindice:stop2_colindice]
 
         comm.send(A_reduced, dest=0, tag=0)
+        comm.send(L_reduced, dest=0, tag=1)
+        comm.send(U_reduced, dest=0, tag=2)
 
 
-
-
-    

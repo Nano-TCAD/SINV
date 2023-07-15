8f4feb98156f4ef05fb46a2bd791d884e8987883
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2023-07-15 15:05:16 +0200

    minor

---
+++ algorithms/pairwise.py
@@ -0,0 +1,47 @@
+"""
+@author: Vincent Maillou (vmaillou@iis.ee.ethz.ch)
+@date: 2023-07
+
+Pairwise 2007 algorithm:
+@reference: https://doi.org/10.1063/1.2748621
+@reference: https://doi.org/10.1063/1.3624612
+
+Pairwise 2018 algorithm:
+@reference: https://doi.org/10.1007/978-3-319-78024-5_55
+
+Copyright 2023 ETH Zurich and the QuaTrEx authors. All rights reserved.
+"""
+
+
+import utils.vizualisation       as vizu
+import utils.transformMatrices   as transMat
+
+import numpy as np
+import math
+import time
+
+from mpi4py import MPI
+
+
+
+
+def pairwise_2007():
+    """
+        Implementation of the pairwise algorithm as described in the 2007 paper
+        and extended in 2011.
+    """
+
+    pass
+
+
+
+
+
+
+
+def pairwise_2018():
+    """
+        Implementation of the pairwise algorithm as described in the 2018 paper.
+    """
+
+    pass

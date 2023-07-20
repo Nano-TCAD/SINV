643f617a08c9582395446cc51548ce2980bd3354
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2023-07-20 15:45:58 +0200

    Implemented: allocate_memory_for_partitions()

--- algorithms/pdiv_parallel.py
+++ algorithms/pdiv_parallel.py
@@ -25,7 +25,7 @@ from mpi4py import MPI
 
 
 
-def allocate_memory_for_partitions(A, n_partitions, blocksize):
+def allocate_memory_for_partitions(A, n_partitions, n_reduction_steps, blocksize):
     """
         Allocate the needed memory to store the current partition of the
         system at each steps of the assembly process.
@@ -37,7 +37,61 @@ def allocate_memory_for_partitions(A, n_partitions, blocksize):
         @return: K_local, B_local, l_start_blockrow, l_partitions_sizes
     """
 
-    pass
+    comm = MPI.COMM_WORLD
+    comm_rank = comm.Get_rank()
+
+    nblocks = A.shape[0] // blocksize
+    partition_blocksize = nblocks // n_partitions
+    blocksize_of_first_partition = nblocks - partition_blocksize * (n_partitions-1)
+
+
+    # Compute the starting block row and the partition size for each process
+    l_start_blockrow   = []
+    l_partitions_sizes = []
+
+    for i in range(n_partitions):
+        if i == 0:
+            l_start_blockrow   = [0]
+            l_partitions_sizes = [blocksize_of_first_partition]
+        else:
+            l_start_blockrow.append(l_start_blockrow[i-1] + l_partitions_sizes[i-1])
+            l_partitions_sizes.append(partition_blocksize)
+
+
+    # Compute the needed memory for the local K matrix and B bridges factors
+    K_havent_been_allocated = True
+    K_number_of_blocks_to_allocate = 0
+    B_number_of_blocks_to_allocate = 0
+
+    # Look at the steps that will be performed backwards: we want to allocate
+    # the biggest container size that will be needed by the process.
+    for current_step in range(n_reduction_steps, -1, -1):
+        
+        # Size of the system that will be assembled at this step (equal to the stride)
+        current_step_partition_blocksize = int(math.pow(2, current_step))
+
+        # Look at each processes that will perform computations at this step
+        for process in range(0, n_partitions, current_step_partition_blocksize):
+            if process == comm_rank:
+                if K_havent_been_allocated:
+                    # Loop through the blocks that will be assembled at this step
+                    # to compute the needed memory for the local K matrix
+                    for i in range(process, process+current_step_partition_blocksize):
+                        K_number_of_blocks_to_allocate += l_partitions_sizes[i]
+                    K_havent_been_allocated = False
+
+                if current_step > 0:
+                    B_number_of_blocks_to_allocate += 1
+
+    # Allocate memory for the local K matrix
+    K_local = np.zeros((K_number_of_blocks_to_allocate*blocksize, K_number_of_blocks_to_allocate*blocksize), dtype=A.dtype)
+
+    # Allocate memory for the local B bridges factors
+    B_local = np.zeros((B_number_of_blocks_to_allocate, blocksize), dtype=A.dtype)
+
+
+    return K_local, B_local, l_start_blockrow, l_partitions_sizes
+                
 
 
 def partition_subdomain(A, l_start_blockrow, l_partitions_sizes, blocksize):
@@ -183,9 +237,9 @@ def pdiv(A, blocksize):
     n_partitions      = comm_size
     n_reduction_steps = int(math.log2(n_partitions))
 
-    K_local, B_local, l_start_blockrow, l_partitions_sizes = allocate_memory_for_partitions(A, n_partitions, blocksize)
+    K_local, B_local, l_start_blockrow, l_partitions_sizes = allocate_memory_for_partitions(A, n_partitions, n_reduction_steps, blocksize)
 
-    if comm_rank == 0:
+    """ if comm_rank == 0:
         K_i, B_i = partition_subdomain(A, l_start_blockrow, l_partitions_sizes, blocksize)
         send_partitions(K_i, l_start_blockrow, l_partitions_sizes, blocksize)
         send_bridges(B_i, n_reduction_steps, blocksize)
@@ -193,7 +247,7 @@ def pdiv(A, blocksize):
         recv_partitions(K_local, l_start_blockrow, l_partitions_sizes, blocksize)
         recv_bridges(B_local, n_reduction_steps, blocksize)
 
-
+    
     # Inversion of the local partition
     invert_partition(K_local, blocksize)
 
@@ -202,11 +256,10 @@ def pdiv(A, blocksize):
     for current_step in range(0, n_reduction_steps):
         assemble_subpartitions(K_local, current_step, n_reduction_steps, blocksize)
         U = compute_update_term(K_local, B_local, current_step, n_reduction_steps, blocksize)
-        update_partition(K_local, U, current_step, n_reduction_steps, blocksize)
+        update_partition(K_local, U, current_step, n_reduction_steps, blocksize) """
     
 
-    return K_local
-
+    return A
 
 
 #assembly_process = [i for i in range(0, n_partitions, int(math.pow(2, current_step)))]

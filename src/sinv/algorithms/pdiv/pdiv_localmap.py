20b27d1104b40b5532508e292f4d806977c0c422
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2023-08-22 14:38:29 +0200

    started re-implementation

--- src/sinv/algorithms/pdiv/pdiv_localmap.py
+++ src/sinv/algorithms/pdiv/pdiv_localmap.py
@@ -23,14 +23,20 @@ from mpi4py import MPI
 
 
 
-def pdiv_localmap(A: np.ndarray, 
-                  blocksize: int) -> np.ndarray:
+def pdiv_localmap(K_local: np.ndarray,
+                  l_upperbridges: np.ndarray,
+                  l_lowerbridges: np.ndarray, 
+                  blocksize: int) -> [np.ndarray, np.ndarray, np.ndarray]:
     """ Parallel Divide & Conquer implementation of the PDIV/Pairwise algorithm.
         
     Parameters
     ----------
-    A : numpy matrix
-        matrix to invert
+    K_local : numpy matrix
+        local partition of the matrix to invert
+    l_upperbridges : numpy matrix
+        list of the upper bridges of the entire matrix
+    l_lowerbridges : numpy matrix
+        list of the lower bridges of the entire matrix
     blocksize : int
         size of a block
 
@@ -38,12 +44,19 @@ def pdiv_localmap(A: np.ndarray,
     -------
     K_local : numpy matrix
         inverted local partition of the matrix
+    l_upperbridges : numpy matrix
+        updated lower bridges (return the entire list of bridges but only the
+        local bridges are updated)
+    l_lowerbridges : numpy matrix
+        updated upper bridges (return the entire list of bridges but only the
+        local bridges are updated)
 
     Notes
     -----
     The PDIV (Pairwise) algorithm is a divide and conquer approch to compute
     the inverse of a matrix. The matrix is divided into submatrices, distributed
-    among the processes, inverted locally and updated thourgh a series of reduction.
+    among the processes, inverted locally and updated in parallel using the 
+    Sherman-Morrison formula.
 
     This implementation perform local update of the distributed partition. Hence
     the inverted system is scattered across the processes.
@@ -57,99 +70,104 @@ def pdiv_localmap(A: np.ndarray,
     comm_rank = comm.Get_rank()
     comm_size = comm.Get_size()
     
-    pdiv_u.check_multiprocessing(comm_size)
-    pdiv_u.check_input(A, blocksize, comm_size)
-    
-    l_start_blockrow, l_partitions_blocksizes = pdiv_u.divide_matrix(A, comm_size, blocksize)
-
-    if comm_rank == 0:
-        print("l_start_blockrow: ", l_start_blockrow)
-        print("l_partitions_blocksizes: ", l_partitions_blocksizes)        
-        
-    start_blockrow = l_start_blockrow[comm_rank]
-    partition_size = l_partitions_blocksizes[comm_rank]
-    K_local, Bu_i, Bl_i = get_local_partition(A, start_blockrow, partition_size, blocksize)
-
     invert_partition(K_local)
 
-    n_partitions      = comm_size
-    n_reduction_steps = int(math.log2(n_partitions))
-    
-    for current_step in range(1, n_reduction_steps+1):
-        processes_stride = int(math.pow(2, current_step))
-        for active_process in range(0, n_partitions, processes_stride):
-            starting_process = active_process
-            ending_process   = starting_process + processes_stride - 1
-            
-            middle_process = get_middle_process(starting_process, ending_process)
-                
-            if comm_rank == 0:
-                print("current_step: ", current_step, "starting_process: ", starting_process, " middle_process: ", middle_process, " ending_process: ", ending_process)
+    l_M = initialize_matrixmaps(blocksize)
+    l_C = initialize_crossmaps(blocksize)
+
+    n_reduction_steps = int(math.log2(comm_size))
+    for current_step in range(1, n_reduction_steps + 1):
+        update_maps(l_M, l_C, K_local, l_upperbridges, l_lowerbridges, current_step, blocksize)
 
 
-    return K_local
+    return K_local, l_upperbridges, l_lowerbridges
 
-
-
-def get_local_partition(A: np.ndarray,
-                        start_blockrow: int,
-                        partition_size: int,
-                        blocksize: int) -> np.ndarray:
-    """
-    
-    """    
-    
-    comm = MPI.COMM_WORLD
-    comm_rank = comm.Get_rank()
-    comm_size = comm.Get_size()
-
-    # Compute the needed memory for the local K matrix and B bridges factors
-    K_number_of_blocks_to_allocate = 0
-
-    # All processes only get allocated the size of their locale partition.
-    K_number_of_blocks_to_allocate = partition_size
-
-    # Allocate memory for the local K matrix
-    K_local = np.zeros((K_number_of_blocks_to_allocate*blocksize, K_number_of_blocks_to_allocate*blocksize), dtype=A.dtype)
-
-    start_rowindex = start_blockrow * blocksize
-    stop_rowindex  = start_rowindex + partition_size * blocksize
-    
-    K_local = A[start_rowindex: stop_rowindex, start_rowindex: stop_rowindex]
-    
-    if comm_rank == 0:
-        Bu_i = A[stop_rowindex-blocksize: stop_rowindex, stop_rowindex: stop_rowindex+blocksize]
-        Bl_i = None
-        
-        return K_local, Bu_i, Bl_i
-    
-    elif comm_rank == comm_size - 1:
-        Bu_i = None
-        Bl_i = A[start_rowindex: start_rowindex+blocksize, start_rowindex-blocksize: start_rowindex]
-        
-        return K_local, Bu_i, Bl_i
-    
-    else:
-        Bu_i = A[stop_rowindex-blocksize: stop_rowindex, stop_rowindex: stop_rowindex+blocksize]
-        Bl_i = A[start_rowindex: start_rowindex+blocksize, start_rowindex-blocksize: start_rowindex]
-        
-        return K_local, Bu_i, Bl_i
-    
-    
     
 def invert_partition(K_local: np.ndarray) -> np.ndarray:
     """
-    
     """
     
     K_local = np.linalg.inv(K_local)
 
 
+def initialize_matrixmaps(blocksize: int) -> list[np.ndarray]:
+    """
+    """
+
+    l_M = []
+
+    for i in range(12):
+        if i == 1 or i == 4 or i == 5 or i == 8:
+            l_M.append(np.identity(blocksize))
+        else:
+            l_M.append(np.zeros((blocksize, blocksize)))
+            
+    return l_M
+
+
+def initialize_crossmaps(blocksize: int) -> list[np.ndarray]:
+    """
+    """
+
+    l_C = [np.zeros((blocksize, blocksize)) for i in range(12)]
+    
+    return l_C
+
+
+def update_maps(l_M: np.ndarray,
+                l_C: np.ndarray,
+                K_local: np.ndarray,
+                l_upperbridges: np.ndarray,
+                l_lowerbridges: np.ndarray,
+                current_step: int,
+                blocksize: int) -> [np.ndarray, np.ndarray]:
+    """
+    """
+
+    comm = MPI.COMM_WORLD
+    comm_rank = comm.Get_rank()
+    comm_size = comm.Get_size()
+    
+    process_stride = int(math.pow(2, current_step))
+    
+    for active_process in range(0, comm_size, process_stride):
+        starting_process = active_process
+        ending_process   = active_process + process_stride - 1
+        middle_process = get_middle_process(starting_process, ending_process)
+
+        l_U = get_U(K_local, l_M, blocksize)
+        
+        """ Bu_mid = l_upperbridges[middle_process]
+        Bl_mid = l_lowerbridges[middle_process]
+        J   = get_J(l_U, Bu_mid, Bl_mid, blocksize) """
+
+        if comm_rank == 0:        
+            print("current_step: ", current_step, " active_process: ", active_process, " starting_process: ", starting_process, " ending_process: ", ending_process, " middle_process: ", middle_process)
+
+
+def get_U(K_local: np.ndarray,
+          l_M: np.ndarray,
+          blocksize: int) -> list[np.ndarray]:
+    """
+    """
+    
+    UUR = np.zeros((blocksize, blocksize)) 
+    ULL = np.zeros((blocksize, blocksize))
+    ULR = np.zeros((blocksize, blocksize))
+    DUL = np.zeros((blocksize, blocksize))
+    DUR = np.zeros((blocksize, blocksize))
+    DLL = np.zeros((blocksize, blocksize))
+    
+    
+    
+    
+    return [UUR, ULL, ULR, DUL, DUR, DLL]
+    
+    
 
 def get_middle_process(starting_process: int, 
                        ending_process: int) -> int:
     """
-    
     """
     
     middle_process = starting_process - 1 + math.ceil((ending_process - starting_process) / 2) 
@@ -173,17 +191,25 @@ if __name__ == '__main__':
     
     if comm_size <= nblocks:
         A = utils.gen_mat.generateBandedDiagonalMatrix(matrice_size, bandwidth, isComplex, seed)
-        
-        
         A_refsol = np.linalg.inv(A)
-        A_pdiv_localmap = pdiv_localmap(A, blocksize)
+        
+        
+        # PDIV worflow
+        pdiv_u.check_multiprocessing(comm_size)
+        pdiv_u.check_input(A, blocksize, comm_size)
         
         l_start_blockrow, l_partitions_blocksizes = pdiv_u.divide_matrix(A, comm_size, blocksize)
+        K_i, Bu_i, Bl_i = pdiv_u.partition_subdomain(A, l_start_blockrow, l_partitions_blocksizes, blocksize)
         
+        K_local = K_i[comm_rank]
+        K_local, Bu_i, Bl_i = pdiv_localmap(K_local, Bu_i, Bl_i, blocksize)
+        
+        
+        # Extract local reference solution
         start_localpart_rowindex = l_start_blockrow[comm_rank] * blocksize
         stop_localpart_rowindex  = start_localpart_rowindex + l_partitions_blocksizes[comm_rank] * blocksize
-        
         A_local_slice_of_refsolution = A_refsol[start_localpart_rowindex:stop_localpart_rowindex, start_localpart_rowindex:stop_localpart_rowindex]
+        
         
         #utils.vizu.compareDenseMatrix(A_local_slice_of_refsolution, f"A_local_slice_of_refsolution\n Process: {comm_rank} "  , A_pdiv_localmap, f"A_pdiv_localmap\n Process: {comm_rank} ")
         

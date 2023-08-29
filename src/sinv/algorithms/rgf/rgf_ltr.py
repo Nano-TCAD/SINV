c942d085c5daa860ee040ec4833ea8a966529e02
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2023-08-29 14:04:57 +0200

    Modified RGF to use the new data structure and to surprisingly compute lower blocks as well.. Life is full of surprises.

--- src/sinv/algorithms/rgf/rgf_ltr.py
+++ src/sinv/algorithms/rgf/rgf_ltr.py
@@ -9,63 +9,91 @@ Copyright 2023 ETH Zurich and the QuaTrEx authors. All rights reserved.
 
 import numpy as np
 
+from quasi.bsparse._base import bsparse
 
 
-def rgf_leftToRight(A_bloc_diag: np.ndarray, 
-                    A_bloc_upper: np.ndarray, 
-                    A_bloc_lower: np.ndarray) -> [np.ndarray, np.ndarray, np.ndarray]:
+
+def rgf_leftToRight(
+    A: bsparse,
+    save_off_diag: bool = True,
+) -> bsparse:
     """ RGF algorithm performing block-tridiagonal selected inversion on the 
-    input matrix. Act from left to right.
+    input matrix. Act from upper left to lower right.
 
     Parameters
     ----------
-    A_bloc_diag : np.ndarray
-        Block-diagonal elements of the matrix to invert.
-    A_bloc_upper : np.ndarray
-        Upper block-diagonal elements of the matrix to invert.
-    A_bloc_lower : np.ndarray
-        Lower block-diagonal elements of the matrix to invert.
+    A : bsparse
+        Input matrix.
+    save_off_diag : bool, optional
+        If True, the off-diagonal blocks are saved in the output matrix.
 
     Returns
     -------
-    G_diag_blocks : np.ndarray
-        Diagonal blocks of the inverse matrix.
-    G_upper_blocks : np.ndarray
-        Upper diagonal blocks of the inverse matrix.
-    G_lower_blocks : np.ndarray
-        Lower diagonal blocks of the inverse matrix.
+    G : bsparse
+        Inverse of the input matrix.
     """
-    
-    nblocks  = A_bloc_diag.shape[0]
-    blockSize = A_bloc_diag.shape[1]
-
-    # Storage for the incomplete forward substitution
-    g_diag_blocks  = np.zeros((nblocks, blockSize, blockSize), dtype=A_bloc_diag.dtype)
 
     # Storage for the full backward substitution 
-    G_diag_blocks  = np.zeros((nblocks, blockSize, blockSize), dtype=A_bloc_diag.dtype)
-    G_upper_blocks = np.zeros((nblocks-1, blockSize, blockSize), dtype=A_bloc_upper.dtype)
-    G_lower_blocks = np.zeros((nblocks-1, blockSize, blockSize), dtype=A_bloc_lower.dtype)
+    G = A.copy() * np.nan
+    
+    # 0. Inverse of the first block
+    G.blocks[0, 0] = np.linalg.inv(A.blocks[0, 0])
+
+    # 1. Forward substitution (performed left to right)
+    for i in range(1, A.blockorder, 1):
+        G.blocks[i, i] = np.linalg.inv(A.blocks[i, i] - A.blocks[i, i-1] @ G.blocks[i-1, i-1] @ A.blocks[i-1, i])
+
+    # 2. Backward substitution (performed right to left)
+    for i in range(A.blockorder-2, -1, -1): 
+        G_ii = G.blocks[i, i]
+        G_upperfactor = G.blocks[i+1, i+1] @ A.blocks[i+1, i] @ G_ii   
+        
+        if save_off_diag:
+            G.blocks[i, i+1] = -G_ii @ A.blocks[i, i+1] @ G.blocks[i+1, i+1]
+            G.blocks[i+1, i] = -G_upperfactor
+            
+        G.blocks[i, i]   =  G_ii + G_ii @ A.blocks[i, i+1] @ G_upperfactor
+            
+
+    return G
 
 
-    # 1. Initialisation of g
-    g_diag_blocks[0, ] = np.linalg.inv(A_bloc_diag[0, ])
 
-    # 2. Forward substitution
-    # From left to right
-    for i in range(1, nblocks):
-        g_diag_blocks[i, ] = np.linalg.inv(A_bloc_diag[i, ] - A_bloc_lower[i-1, ] @ g_diag_blocks[i-1, ] @ A_bloc_upper[i-1, ])
+from quasi.bsparse import bdia, bsr, vbdia, vbsr
 
-    # 3. Initialisation of last element of G
-    G_diag_blocks[-1, ] = g_diag_blocks[-1, ]
+if __name__ == '__main__':
+    SIZE = 4
+    BLOCKSIZE = 2
 
-    # 4. Backward substitution
-    # From right to left
-    for i in range(nblocks-2, -1, -1): 
-        G_diag_blocks[i, ]  =  g_diag_blocks[i, ] @ (np.identity(blockSize) + A_bloc_upper[i, ] @ G_diag_blocks[i+1, ] @ A_bloc_lower[i, ] @ g_diag_blocks[i, ])
-        G_upper_blocks[i, ] = -g_diag_blocks[i, ] @ A_bloc_upper[i, ] @ G_diag_blocks[i+1, ]
-        G_lower_blocks[i, ] =  G_upper_blocks[i, ].T
+    a = bsr.diag(
+        [np.random.rand(SIZE, SIZE) for _ in range(10)], BLOCKSIZE, BLOCKSIZE
+    )
+    x_rgf = rgf_leftToRight(a)
+    x_inv = np.linalg.inv(a.toarray())
 
+    # Main Diagonal.
+    for i in range(x_rgf.blockorder):
+        ii = slice(i * BLOCKSIZE, (i + 1) * BLOCKSIZE)
+        assert np.allclose(x_rgf.blocks[i, i], x_inv[ii, ii])
+        
+    import matplotlib.pyplot as plt
+    
+    # Create a subplot that show the two plots side by side
+    #ax = plt.subplots(1, 2)
+    """ x_rgf.show()
+    plt.matshow(np.abs(x_inv))
+    plt.show() """
+    
+    
+    """ plt.subplot(1, 2, 1)
+    plt_axe_0 = x_rgf.show()
+    plt.subplot(1, 2, 2)
+    plt_axe_1 = plt.matshow(np.abs(x_inv))
+    plt.show() """
 
-    return G_diag_blocks, G_upper_blocks, G_lower_blocks
-
+    # Off-Diagonals.
+    for i in range(x_rgf.blockorder - 1):
+        ii = slice(i * BLOCKSIZE, (i + 1) * BLOCKSIZE)
+        jj = slice((i + 1) * BLOCKSIZE, (i + 2) * BLOCKSIZE)
+        assert np.allclose(x_rgf.blocks[i + 1, i], x_inv[jj, ii])
+        assert np.allclose(x_rgf.blocks[i, i + 1], x_inv[ii, jj])

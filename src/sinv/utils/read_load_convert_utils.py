ebfb87efc7cb8e29456088119177d531dc19b522
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2024-01-16 14:38:44 +0100

    added read local partition utils function

--- src/sinv/utils/read_load_convert_utils.py
+++ src/sinv/utils/read_load_convert_utils.py
@@ -11,22 +11,15 @@ import numpy as np
 
 def read_block_tridiagonal_matrix(
     file_path: str,
-    n_blocks: int,
-    blocksize: int,
-    is_complex: bool,
 ) -> [np.ndarray, np.ndarray, np.ndarray]:
-    """ Read a block tridiagonal matrix stored in .npz format.
+    """ Read a block tridiagonal matrix stored in .npz format. First stored array 
+    should contain the diagonal blocks, second array the upper diagonal blocks 
+    and third array the lower diagonal blocks.
     
     Parameters
     ----------
     file_path : str
         Path to the file where the matrix is stored.
-    n_blocks : int
-        Number of blocks of the matrix.
-    blocksize : int
-        Size of the blocks.
-    is_complex : bool
-        Whether the matrix is complex or real valued.
         
     Returns
     -------
@@ -41,24 +34,74 @@ def read_block_tridiagonal_matrix(
         
     matrix = np.load(file_path)
     
-    diagonal_blocks = matrix["diagonal_blocks"]
-    upper_diagonal_blocks = matrix["upper_diagonal_blocks"]
-    lower_diagonal_blocks = matrix["lower_diagonal_blocks"]
+    diagonal_blocks = matrix[matrix.files[0]]
+    upper_diagonal_blocks = matrix[matrix.files[1]]
+    lower_diagonal_blocks = matrix[matrix.files[2]]
     
     return diagonal_blocks, upper_diagonal_blocks, lower_diagonal_blocks
-
 
 
 def read_local_block_tridiagonal_partition(
     file_path: str,
     start_blockrow: int,
     partition_size: int,
-    include_bridges_blocks: bool,
-    n_blocks: int,
     blocksize: int,
-    is_complex: bool,
+    include_bridges_blocks: bool = False,
 ) -> [np.ndarray, np.ndarray, np.ndarray]:
-    pass
+    """ Read a local partition of a block tridiagonal matrix stored in .npz format.
+    First stored array should contain the diagonal blocks, second array the upper 
+    diagonal blocks and third array the lower diagonal blocks.
+    
+    Optionally include the bridge blocks (upper and lower) conecting the partition
+    to it's neighbours.
+    
+    Parameters
+    ----------
+    file_path : str
+        Path to the file where the matrix is stored.
+    start_blockrow : int
+        Index of the first blockrow of the local partition in the global matrix.
+    partition_size : int
+        Size of the local partition.
+    blocksize : int
+        Size of the blocks.
+    include_bridges_blocks : bool, optional
+        Whether to include the bridge blocks (upper and lower) conecting the 
+        partition to it's neighbours. The default is False.
+        
+    Returns
+    -------
+    diagonal_blocks : np.ndarray
+        The diagonal blocks of the matrix.
+    upper_diagonal_blocks : np.ndarray
+        The upper diagonal blocks of the matrix.
+    lower_diagonal_blocks : np.ndarray
+        The lower diagonal blocks of the matrix.
+    """
+    
+    matrix = np.load(file_path)
+    
+    diagonal_blocks = matrix[matrix.files[0]][start_blockrow*blocksize:(start_blockrow+partition_size)*blocksize,:]
+
+    off_diagonal_number_of_blocks = partition_size - 1 
+    
+    start_upper_blockrow = start_blockrow
+    start_lower_blockrow = start_blockrow
+    
+    stop_upper_blockrow = start_blockrow + off_diagonal_number_of_blocks
+    stop_lower_blockrow = start_blockrow + off_diagonal_number_of_blocks
+    
+    if include_bridges_blocks:
+        start_upper_blockrow = max(0, start_blockrow-1)
+        start_lower_blockrow = max(0, start_blockrow-1)
+        
+        stop_upper_blockrow = min(start_blockrow + partition_size, matrix[matrix.files[1]].shape[0]//blocksize)
+        stop_lower_blockrow = min(start_blockrow + partition_size, matrix[matrix.files[2]].shape[0]//blocksize)
+
+    upper_diagonal_blocks = matrix[matrix.files[1]][start_upper_blockrow*blocksize:stop_upper_blockrow*blocksize,:]
+    lower_diagonal_blocks = matrix[matrix.files[2]][start_lower_blockrow*blocksize:stop_lower_blockrow*blocksize,:]
+
+    return diagonal_blocks, upper_diagonal_blocks, lower_diagonal_blocks
 
 
 def read_csr_matrix(
--- src/sinv/utils/sandbox.py
+++ src/sinv/utils/sandbox.py
@@ -1,30 +1,53 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from read_load_convert_utils import read_block_tridiagonal_matrix
+from read_load_convert_utils import read_block_tridiagonal_matrix, read_local_block_tridiagonal_partition
 from testing_utils import create_block_tridiagonal_matrix, save_block_tridigonal_matrix
 from partition_utils import get_local_partition_indices
 
+
+# if __name__ == "__main__":
+    
+#     n_blocks = 10
+#     blocksize = 4
+#     is_complex = False
+    
+#     diagonal_blocks, upper_diagonal_blocks, lower_diagonal_blocks = create_block_tridiagonal_matrix(n_blocks, blocksize, is_complex)
+        
+#     save_block_tridigonal_matrix(diagonal_blocks, upper_diagonal_blocks, lower_diagonal_blocks, "test.npz")
+    
+#     loaded_diagonal_blocks, loaded_upper_diagonal_blocks, loaded_lower_diagonal_blocks = read_block_tridiagonal_matrix("test.npz")
+    
+#     np.allclose(diagonal_blocks, loaded_diagonal_blocks)
+#     np.allclose(upper_diagonal_blocks, loaded_upper_diagonal_blocks)
+#     np.allclose(lower_diagonal_blocks, loaded_lower_diagonal_blocks)
+   
+
+   
 if __name__ == "__main__":
     
     n_blocks = 10
-    blocksize = 4
+    blocksize = 1
     is_complex = False
     
     diagonal_blocks, upper_diagonal_blocks, lower_diagonal_blocks = create_block_tridiagonal_matrix(n_blocks, blocksize, is_complex)
         
     save_block_tridigonal_matrix(diagonal_blocks, upper_diagonal_blocks, lower_diagonal_blocks, "test.npz")
-    
-    # loaded_diagonal_blocks, loaded_upper_diagonal_blocks, loaded_lower_diagonal_blocks = read_block_tridiagonal_matrix("test.npz", n_blocks, blocksize, is_complex)
-    
-    # np.allclose(diagonal_blocks, loaded_diagonal_blocks)
-    # np.allclose(upper_diagonal_blocks, loaded_upper_diagonal_blocks)
-    # np.allclose(lower_diagonal_blocks, loaded_lower_diagonal_blocks)
-    
+     
     partition_number = 0
     n_partitions = 4
     partitions_blocksizes = [3, 2, 2, 3]
+    bridges_blocks = True
     
     start_blockrow, end_blockrow, partition_size = get_local_partition_indices(partition_number, n_partitions, partitions_blocksizes)
     
-    print(start_blockrow, end_blockrow, partition_size)
+    loaded_diagonal_blocks, loaded_upper_diagonal_blocks, loaded_lower_diagonal_blocks = read_local_block_tridiagonal_partition("test.npz", start_blockrow, partition_size, blocksize, include_bridges_blocks=bridges_blocks)
+    
+    fig, axs = plt.subplots(3, 2)
+    axs[0, 0].matshow(diagonal_blocks)
+    axs[0, 1].matshow(loaded_diagonal_blocks)
+    axs[1, 0].matshow(upper_diagonal_blocks)
+    axs[1, 1].matshow(loaded_upper_diagonal_blocks)
+    axs[2, 0].matshow(lower_diagonal_blocks)
+    axs[2, 1].matshow(loaded_lower_diagonal_blocks)
+    plt.show()
